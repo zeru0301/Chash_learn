67c7811f921c65470148c8ea069fb6b17b1170d7
zeru0301 <airisk1124@icloud.com>
2025-11-07 13:21:20 +0900

    c#のフレームワークASP.NETを学習したものの軌跡です

---
+++ sample02/sample02/Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sample02.Models;
+
+namespace sample02.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name, int age)
+        {
+            //モデルにでーたをつめる 
+            var user = new UserModel
+            {
+                Name = name,
+                Age = age
+            };
+            //ビューにモデルを渡す
+            return View(user);
+
+        }
+    }
+}
---
+++ sample02/sample02/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace sample02.Models
+{
+    public class UserModel
+    {
+        public string? Name {  get; set; }
+        public int? Age {  get; set; }
+    }
+}
